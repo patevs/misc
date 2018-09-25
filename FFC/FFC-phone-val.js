7ec934f7e808651966d430f7d072a018290ef5d5
patevs <ppevans11@gmail.com>
2018-09-25 21:47:12 +1200

    updates FCC

--- FFC/FFC-phone-val.js
+++ FFC/FFC-phone-val.js
@@ -2,10 +2,11 @@ function telephoneCheck(str) {
   // Good luck!
   // result to return
   var result = true;
-  // get string length
+  // count string length
   var size = str.length;
   // check pre-reqs
-  if(size < 9 ){ return false; }
+  if(size < 8 ){ return false; }
+  var arr = [1,2,3,4,5,6,7,8,9,0];
 
   // check for open and closing brackets
   var bracket = false;
@@ -19,13 +20,20 @@ function telephoneCheck(str) {
     bracket = true;
   }
   if(bracket){
-    str.replace("(", " ");
-    str.replace(")", " ");
+    str.replace("(", "");
+    str.replace(")", "");
   }
   // check if dashed
   if(str.includes("-")){
     if(str[0] === "-"){ return false; }
-    str.replace("-", " ");
+    str.replace("-", "");
+  }
+
+  for(var i=0;i<size-1;i++){
+    var s = str[i];
+    if(s.isNum){
+        return true;
+    }
   }
 
   // return result
