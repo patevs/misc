32f45a7e42460efbbe0a3b3365251250970e7a1f
patevs <ppevans11@gmail.com>
2018-10-06 17:00:21 +1300

    updates to m-rs

--- m-rs/src/Jumbo/Jumbo.js
+++ m-rs/src/Jumbo/Jumbo.js
@@ -1,17 +1,24 @@
+
+// react component library import
 import React, { Component } from 'react';
 
-import { Jumbotron } from 'react-bootstrap'
+// import bootstrap components; jumbotron, image
+import { Jumbotron, Image } from 'react-bootstrap'
 
+// import Jumbotron style sheet
 import './Jumbo.css';
 
+// Jumbo Header react component
 class Jumbo extends Component {
   render() {
     return (
       <div className="Jumbo">
 
-		<Jumbotron id="tron">
-		  <img src="./assets/imgs/jumbo.png" alt="jumbo header" responsive="true" />
-		</Jumbotron>	
+    		<Jumbotron id="tron">
+
+          <Image src="./assets/imgs/Jumbo.png" alt="jumbo header" responsive />
+
+    		</Jumbotron>
 
       </div>
     );
