8ffa0dad1e5d90aec53d2eb091c9af8feaeeb61b
patevs <ppevans11@gmail.com>
2018-11-01 10:58:58 +1300

    tracking profiles/

---
+++ profiles/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,106 @@
+
+######################
+# PowerShell Profile #
+######################
+
+# http://serverfault.com/questions/95431
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt {
+    # https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
+    $origLastExitCode = $LastExitCode
+    Write-VcsStatus
+
+    if (Test-Administrator) {  # if elevated
+        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+    }
+
+    Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+    Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    }
+
+    Write-Host $curPath -NoNewline -ForegroundColor Blue
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    $LastExitCode = $origLastExitCode
+    "`n$('>' * ($nestedPromptLevel + 1)) "
+}
+
+Import-Module posh-git
+#Import-Module -Name posh-git
+Import-Module Get-ChildItemColor
+
+Start-SshAgent
+
+Set-Alias dir Get-ChildItemColor -option AllScope -Force
+Set-Alias ls Get-ChildItemColor -option AllScope -Force
+Set-Alias ll Get-ChildItemColorFormatWide -option AllScope
+
+function get-gitstatus { git status }
+Set-Alias -Name gs -Value get-gitstatus
+
+<#
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    Write-Host
+
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    if (Test-Administrator) {  # Use different username if elevated
+        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+    }
+
+    Write-Host "$ENV:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+    Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+
+    if ($s -ne $null) {  # color for PSSessions
+        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+        Write-Host ") " -NoNewline -ForegroundColor DarkGray
+    }
+
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\','\\'), "~") -NoNewline -ForegroundColor Blue
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+
+    Write-VcsStatus
+
+    Write-Host ""
+
+    return "> "
+}
+#>
+
+
+<#
+$env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+[AppDomain]::CurrentDomain.GetAssemblies() | % {
+  if (! $_.location) {continue}
+  $Name = Split-Path $_.location -leaf
+  Write-Host -ForegroundColor Yellow "NGENing : $Name"
+  ngen install $_.location | % {"`t$_"}
+}
+#>
+
+
