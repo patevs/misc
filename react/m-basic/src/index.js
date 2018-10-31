343b837833b152ab57583ca3b7bb3447de7da3ef
patevs <ppevans11@gmail.com>
2018-10-31 19:12:52 +1300

    commit react/m-basic

--- react/m-basic/src/index.js
+++ react/m-basic/src/index.js
@@ -10,12 +10,16 @@ import ReactDOM from 'react-dom';
 // import main style sheet
 import './index.css';
 
+// import jumbo component
+import Jumbo from './components/Jumbo/Jumbo';
 // import app component
 import App from './App';
 
 // import service worker
 import * as serviceWorker from './serviceWorker';
 
+// render jumbo component
+ReactDOM.render(<Jumbo />, document.getElementById('jumbo'));
 // render app component
 ReactDOM.render(<App />, document.getElementById('root'));
 
