7b4c156e489a0edfb3f6078b1a85f4f87d2a1901
patevs <ppevans11@gmail.com>
2018-11-01 14:11:28 +1300

    added UI component to m-basic-ui

---
+++ react/03_m-basic-ui/src/app/components/UI/UI.js
@@ -0,0 +1,24 @@
+
+/*
+  src/app/components/UI/UI.js
+  UI component styles
+*/
+
+// react component library import
+import React, { Component } from 'react';
+
+// import UI style sheet
+import './UI.css';
+
+// UI react component
+class UI extends Component {
+  render() {
+    return (
+      <div className="UI">
+        <h3> UI </h3>
+      </div>
+    );
+  }
+}
+
+export default UI;
