0df52221fc27b0f1e06b1c6eb9f79a3dd12bd8c8
Patrick <ppevans11@gmail.com>
2018-11-13 18:13:40 +1300

    update to terminal app

---
+++ rust/terminal_app/src/main.rs
@@ -0,0 +1,15 @@
+
+/*
+    terminal_app
+    src/main.rs
+*/
+
+extern crate colored;
+
+use colored::*;
+
+// main function
+fn main() {
+    println!("Hello, world!");
+    println!("{} {} !", "it".green(), "works".blue().bold());
+}
