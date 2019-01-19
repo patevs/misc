d7802264b53082e35408e9b5f29246fe100da921
patevs <ppevans11@gmail.com>
2019-01-19 18:02:36 +1300

    added flutter/friendly_chat

---
+++ flutter/3_friendly_chat/lib/main.dart
@@ -0,0 +1,25 @@
+
+/**
+ *  lib/main.dart
+ */
+
+// IMPORTS
+import 'package:flutter/material.dart';
+
+// Application entry point
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Friendly Chat",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Friendy Chat")
+        )
+      )
+    )
+  );
+}
+
+
+// EOF
+
