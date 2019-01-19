ca25c8e1662813bc2d7308a4be4a3c46ea12bd4f
patevs <ppevans11@gmail.com>
2019-01-19 18:52:47 +1300

    flutter/friendly_chat UI updates

--- flutter/3_friendly_chat/lib/main.dart
+++ flutter/3_friendly_chat/lib/main.dart
@@ -18,25 +18,65 @@ class FriendlyChatApp extends StatelessWidget {
     return new MaterialApp(
       title: "Friendly Chat",
       theme: new ThemeData(
-        primarySwatch: Colors.green
+        primarySwatch: Colors.green,
+        accentColor: Colors.greenAccent
       ),
       home: new ChatScreen()
     );
   }
 }
 
-// chat screen stateless widget class
-class ChatScreen extends StatelessWidget {
+// chat screen statelful widget class
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+// chat screen state class
+class ChatScreenState extends State<ChatScreen> {
+  // text controller
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendly Chat")
-      )
+      ),
+      body: _buildTextComposer(),
     );
   }
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                  hintText: "Send a message"),
+              ),
+            ),
+            Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 }
-
+    
 
 // EOF
 
