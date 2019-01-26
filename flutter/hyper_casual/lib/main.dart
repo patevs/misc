61ec56ede2a9a6567dae491743b1f2ef6275679e
Patrick <ppevans11@gmail.com>
2019-01-26 18:23:46 +1300

    updates to flutter/hyper_casual

--- flutter/hyper_casual/lib/main.dart
+++ flutter/hyper_casual/lib/main.dart
@@ -6,16 +6,23 @@
 // IMPORTS
 import 'package:flutter/material.dart';
 
-//import 'package:flame/flame.dart';
-
 import 'dart:math' as math;
 import 'dart:ui';
 
 // flame libs
+import 'package:flame/flame.dart';
 import 'package:flame/anchor.dart';
 import 'package:flame/components/component.dart';
+import 'package:flame/components/text_box_component.dart';
+import 'package:flame/components/text_component.dart';
+import 'package:flame/text_config.dart';
 import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
+
+// Text style configs
+TextConfig regular = TextConfig(color: BasicPalette.white.color);
+TextConfig small = regular.withFontSize(16.0);
+TextConfig tiny = regular.withFontSize(12.0);
 
 // Application entry point
 void main() => runApp(
@@ -29,6 +36,29 @@ class Palette {
   static const PaletteEntry red = PaletteEntry(Color(0xFFFF0000));
   static const PaletteEntry blue = PaletteEntry(Color(0xFF0000FF));
   static const PaletteEntry green = PaletteEntry(Colors.green);
+}
+
+// MyTextBox component class
+class MyTextBox extends TextBoxComponent {
+  // CONSTRUCTOR
+  MyTextBox(String text) 
+    : super(
+        text, 
+        config: tiny, 
+        boxConfig: TextBoxConfig(timePerChar: 0.05)
+      );
+
+  @override
+  void drawBackground(Canvas c) {
+    Rect rect = Rect.fromLTWH(0, 0, width, height);
+    c.drawRect(rect, new Paint()..color = Color(0xFFFF00FF));
+    c.drawRect(
+      rect.deflate(boxConfig.margin), 
+      new Paint()
+        ..color = BasicPalette.black.color
+        ..style = PaintingStyle.stroke
+    );
+  }
 }
 
 // Square component class
@@ -67,7 +97,40 @@ class Square extends PositionComponent {
 class HyperCasualGame extends BaseGame {
   // CONSTRUCTOR
   HyperCasualGame() {
+    _start();
+  }
+
+  _start() async {
+    // get screen dimensions
+    Size size = await Flame.util.initialDimensions();
+    // adding components
+    add(TextComponent('HYPER CASUAL', config: regular)
+      ..anchor = Anchor.topCenter
+      ..x = size.width / 2
+      ..y = 64.0
+    );
     add(new Square(128.0));
+    add(TextComponent('center', config: small)
+      ..anchor = Anchor.center
+      ..x = size.width / 2
+      ..y = size.height / 2
+    );
+    add(TextComponent('bottomLeft', config: tiny)
+      ..anchor = Anchor.bottomLeft
+      ..x = 16.0
+      ..y = size.height - 16.0
+    );
+    add(TextComponent('bottomRight', config: tiny)
+      ..anchor = Anchor.bottomRight
+      ..x = size.width - 16.0
+      ..y = size.height - 16.0
+    );
+    /*
+    add(MyTextBox('bottomLeft')
+      ..anchor = Anchor.bottomLeft
+      ..y = size.height
+    );
+    */
   }
 }
 
