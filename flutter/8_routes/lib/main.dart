14ba2599e3d2be9fa29a9e513f7f7590803b6a80
Patrick <ppevans11@gmail.com>
2019-01-30 21:27:43 +1300

    updates to 8_routes 0

--- flutter/8_routes/lib/main.dart
+++ flutter/8_routes/lib/main.dart
@@ -15,44 +15,44 @@ class RoutesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Routes',
+      title: 'Testing Route Navigation',
       theme: ThemeData(
         // This is the theme of your application.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.lightGreen,
+        primaryColor: Colors.lightGreen,
+        buttonColor: Colors.lightGreenAccent,
       ),
-      home: LoadingScreen(title: 'Navigating Routes - Loading Screen'),
+      home: LoadingScreen(),
     );
   }
 } // END of LoadingApp
 
 // LoadingScreen stateless widget class
 class LoadingScreen extends StatelessWidget {
-  // FIELDS
-  final String title;
-  // CONSTRUCTOR
-  LoadingScreen({this.title});
   // METHODS
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Loading Screen...'),
-          RaisedButton(
-            child: Text('Continue'),
-            onPressed: () {
-              // Navigate to WelcomeScreen
-              Navigator.push(
-                context, 
-                MaterialPageRoute(
-                  builder: (context) => WelcomeScreen(title: 'Navigating Routes - Welcome Screen')
-                ),
-              );
-            },
-          )
-        ],
+      appBar: AppBar(
+        title: Text('LOADING SCREEN'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('--- LOADING SCREEN CONTENT ---'),
+            RaisedButton(
+              child: Text('START'),
+              onPressed: () {
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => WelcomeScreen())
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
@@ -60,31 +60,77 @@ class LoadingScreen extends StatelessWidget {
 
 // WelcomeScreen stateless widget class
 class WelcomeScreen extends StatelessWidget {
-  // FIELDS
-  final String title;
-  // CONSTRUCTOR
-  WelcomeScreen({this.title});
   // METHODS
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Welcome Screen...'),
-          RaisedButton(
-            child: Text('Go Back'),
-            onPressed: () {
-              // Navigate back to LoadingScreen
-              Navigator.pop(context);
-            },
-          )
-        ],
+      appBar: AppBar(
+        title: Text('WELCOME SCREEN'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('--- WELCOME SCREEN CONTENT ---'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('BACK'),
+                  color: Colors.lightBlueAccent,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                RaisedButton(
+                  child: Text('CONTINUE'),
+                  onPressed: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => TestScreen())
+                    );
+                  },
+                ),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+} // END of WelcomeScreen
+
+// TestScreen stateless widget class
+class TestScreen extends StatelessWidget {
+  // METHODS
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TEST SCREEN'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('--- TEST SCREEN CONTENT ---'),
+            RaisedButton(
+              child: Text('Back'),
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
-}
+} // END of TestScreen
 
 
 // EOF
