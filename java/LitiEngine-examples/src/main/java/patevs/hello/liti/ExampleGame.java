5ffd76fed7fdbfd4c9bf54cc033bd9ec93b71bb3
Patrick <ppevans11@gmail.com>
2019-03-08 15:46:57 +1300

    added ExampleGame

---
+++ java/LitiEngine-examples/src/main/java/patevs/hello/liti/ExampleGame.java
@@ -0,0 +1,21 @@
+package patevs.hello.liti;
+
+import de.gurkenlabs.litiengine.Game;
+
+/**
+ * 	Example LITIengine game
+ * 
+ * @author Patrick
+ */
+public class ExampleGame {
+
+	/**
+	 * Main method - Application entry point
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Game.init(args);
+		Game.start();
+	}
+	
+}
