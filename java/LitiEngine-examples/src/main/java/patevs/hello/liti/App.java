c482f2187600e28408904de972db659bcbae8730
Patrick <ppevans11@gmail.com>
2019-03-08 15:31:17 +1300

    added java/LitiEngine-examples

---
+++ java/LitiEngine-examples/src/main/java/patevs/hello/liti/App.java
@@ -0,0 +1,13 @@
+package patevs.hello.liti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
